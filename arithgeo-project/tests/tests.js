fb60eb8d29825b6c8a8f20b39f26d69562d627c6
Bogadeji <Bogadeji>
2016-11-24 23:23:39 +0100

    Modiying ArithGeo Test files

--- arithgeo-project/tests/tests.js
+++ arithgeo-project/tests/tests.js
@@ -11,7 +11,7 @@ var myApp = require('../app/library.js');
     describe("Case for an empty array", function() {
 
       it("should return 0 for an empty array", function() {
-        expect(myApp.aritGeo([])).to.eql(0);
+        expect(myApp.arithGeo([])).to.eql(0);
       });
 
     });
@@ -19,15 +19,15 @@ var myApp = require('../app/library.js');
     describe("Case for an arithmetic sequence", function() {
 
       it("should return `Arithmetic` for [2, 4, 6, 8, 10]", function() {
-        expect(myApp.aritGeo([2, 4, 6, 8, 10])).to.eql('Arithmetic');
+        expect(myApp.arithGeo([2, 4, 6, 8, 10])).to.eql('Arithmetic');
       });
 
       it("should return `Arithmetic` for [5, 11, 17, 23, 29, 35, 41]", function() {
-        expect(myApp.aritGeo([5, 11, 17, 23, 29, 35, 41])).to.eql('Arithmetic');
+        expect(myApp.arithGeo([5, 11, 17, 23, 29, 35, 41])).to.eql('Arithmetic');
       });
 
       it("should return `Arithmetic` for [15, 10, 5, 0, -5, -10]", function() {
-        expect(myApp.aritGeo([15, 10, 5, 0, -5, -10])).to.eql('Arithmetic');
+        expect(myApp.arithGeo([15, 10, 5, 0, -5, -10])).to.eql('Arithmetic');
       });
 
     });
@@ -35,15 +35,15 @@ var myApp = require('../app/library.js');
     describe("Case for a geometric sequence", function() {
 
       it("should return `Geometric` for [2, 6, 18, 54, 162]", function() {
-        expect(myApp.aritGeo([2, 6, 18, 54, 162])).to.eql('Geometric');
+        expect(myApp.arithGeo([2, 6, 18, 54, 162])).to.eql('Geometric');
       });
 
       it("should return `Geometric` for [0.5, 3.5, 24.5, 171.5]", function() {
-        expect(myApp.aritGeo([0.5, 3.5, 24.5, 171.5])).to.eql('Geometric');
+        expect(myApp.arithGeo([0.5, 3.5, 24.5, 171.5])).to.eql('Geometric');
       });
 
       it("should return `Geometric` for [−128, 64, −32, 16, −8]", function() {
-        expect(myApp.aritGeo([-128, 64, -32, 16, -8])).to.eql('Geometric');
+        expect(myApp.arithGeo([-128, 64, -32, 16, -8])).to.eql('Geometric');
       });
 
     });
@@ -51,15 +51,15 @@ var myApp = require('../app/library.js');
     describe("Case for neither arithmetic nor geometric sequence", function() {
 
       it("should return -1 for [1, 2, 3, 5, 8]", function() {
-        expect(myApp.aritGeo([1, 2, 3, 5, 8])).to.eql(-1);
+        expect(myApp.arithGeo([1, 2, 3, 5, 8])).to.eql(-1);
       });
 
       it("should return -1 for [1, 3, 6, 10, 15]", function() {
-        expect(myApp.aritGeo([1, 3, 6, 10, 15])).to.eql(-1);
+        expect(myApp.arithGeo([1, 3, 6, 10, 15])).to.eql(-1);
       });
 
       it("should return -1 for [1, 8, 27, 64, 125]", function() {
-        expect(myApp.aritGeo([1, 8, 27, 64, 125])).to.eql(-1);
+        expect(myApp.arithGeo([1, 8, 27, 64, 125])).to.eql(-1);
       });
       
     });
